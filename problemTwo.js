236acd83e6e7a0490c7655fb89f87b604ac33cc2
Mahesh Cheemalapati <mcheemalapati@issi-software.com>
2019-07-12 09:54:13 -0400

    changes to Fibonacci Sequence

--- problemTwo.js
+++ problemTwo.js
@@ -1,18 +1,22 @@
 console.log("Fibonacci Sequence");
 function FibonacciSequence(firstNum, secondNum, range){
-    let newNum;
+    var newNum;
     let sum = 0;
+    let sumView = [];
     let result = [firstNum, secondNum];
+
     for(var i=0; i<range; i++){
         newNum = firstNum + secondNum;
         firstNum = secondNum;
         secondNum = newNum;
         result.push(newNum);
-        if (newNum%2 === 0){
-            sum += newNum;
+        if (result[i]%2 === 0){
+            sum = sum + result[i];
+            sumView.push(sum);
         }
     }
-    console.log(sum);
+
+    console.log(sumView, sum);
     console.log(result);
 }
-FibonacciSequence(1,2,20);
+FibonacciSequence(1,2,15);
